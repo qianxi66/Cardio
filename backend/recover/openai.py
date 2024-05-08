789411b5057cf31c5a5aa6e6d2df06cf87fd3c40
Yuxuan Lu <luyuxuanleo@gmail.com>
2024-05-08 00:46:08 -0400

    Update prompt, add CONVERSATION_END indicator

--- backend/recover/openai.py
+++ backend/recover/openai.py
@@ -1,8 +1,10 @@
+import json
 from pathlib import Path
 
-import openai, json
+import openai
 
-from .config import openai_config, openai_key, symptom_descriptions
+
+from .config import openai_key, symptom_descriptions
 
 # client = openai.AzureOpenAI(**openai_config)
 client = openai.OpenAI(api_key=openai_key)
@@ -15,11 +17,12 @@ key_questions_prompt = open(key_questions_prompt_path, "r").read(10000000)
 summary_prompt_path = Path(__file__).with_name("summary_prompt.txt")
 summary_prompt = open(summary_prompt_path, "r").read(10000000)
 
+
 def gpt_inference(client: openai.OpenAI, messages, stop=None):
     response = client.chat.completions.create(
         model="gpt-4-turbo",
         messages=messages,
-        max_tokens=512,
+        max_tokens=128,
         stop=stop,
     )
     return response.choices[0].message.content
@@ -31,19 +34,28 @@ def conversation(messages):
         [{"role": "system", "content": conversation_system_prompt}, *messages],
     )
 
+
 def key_questions(messages):
     return gpt_inference(
         client,
-        [{"role": "system", "content": key_questions_prompt}, {
-            "role": "user", "content": messages}],
+        [
+            {"role": "system", "content": key_questions_prompt},
+            {"role": "user", "content": messages},
+        ],
     )
+
 
 def summary(messages, key_questions):
     return gpt_inference(
         client,
-        [{"role": "system", "content": summary_prompt}, 
-         {"role": "user", "content": "List of all symptoms descriptions"+ json.dumps(symptom_descriptions)},
-         {
-            "role": "user", "content": 'messages: ' + messages},
-            {"role": "user", "content": 'symptoms: ' + key_questions}],
+        [
+            {"role": "system", "content": summary_prompt},
+            {
+                "role": "user",
+                "content": "List of all symptoms descriptions"
+                + json.dumps(symptom_descriptions),
+            },
+            {"role": "user", "content": "messages: " + messages},
+            {"role": "user", "content": "symptoms: " + key_questions},
+        ],
     )
