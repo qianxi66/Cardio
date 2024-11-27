78787a7dea8c3fce9f2ec747fa22a53406fdf0a3
Yuxuan Lu <luyuxuanleo@gmail.com>
2024-11-26 17:43:13 -0800

    fix

--- backend/recover/migrations/versions/a41aaf6ffc45_.py
+++ backend/recover/migrations/versions/a41aaf6ffc45_.py
@@ -9,7 +9,51 @@ Create Date: 2024-10-18 22:47:49.988968
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.sql import table, column
-from recover.symptoms import symptom_descriptions
+
+symptom_descriptions = {
+    "activity": {
+        "description": "Decrease in Daily Activities",
+        "display_name": "Activity",
+        "max_scale": 2,
+        "color": "#6283ac",
+        "likert": False,
+    },
+    "cognition": {
+        "description": "Decrease in Cognitive Level",
+        "display_name": "Cognition",
+        "max_scale": 4,
+        "color": "#eb4c44",
+        "likert": False,
+    },
+    "eating": {
+        "description": "Inability to Tolerate Food or Drink",
+        "display_name": "Eating",
+        "max_scale": 3,
+        "color": "#f9d965",
+        "likert": False,
+    },
+    "pain": {
+        "description": "Have you been feeling any discomfort, pain or sore today?",
+        "display_name": "Pain",
+        "max_scale": 3,
+        "color": "#f9d965",
+        "likert": True,
+    },
+    "mood": {
+        "description": "Feeling Down or Depressed",
+        "display_name": "Mood",
+        "max_scale": 2,
+        "color": "#6283ac",
+        "likert": False,
+    },
+    "misc": {
+        "description": "Other Symptoms",
+        "display_name": "Misc",
+        "max_scale": 3,
+        "color": "#ff33db",
+        "likert": False,
+    },
+}
 
 
 # revision identifiers, used by Alembic.
